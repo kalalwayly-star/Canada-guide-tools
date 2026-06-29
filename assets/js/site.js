83802eb8b145f212330e2b3693a92e34943395b4
Kaled Al <kalalwayly@gmail.com>
2026-06-29 04:55:38 -0500

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,6 @@
-
+fetch("data/site.json")
+    .then(res => res.json())
+    .then(site => {
+        document.getElementById("site-name").textContent = site.siteName;
+        document.getElementById("site-tagline").textContent = site.siteTagline;
+    });
