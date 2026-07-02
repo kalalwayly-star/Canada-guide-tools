72cf825fa37d104510e200dbdf553cfb6f68349e
Kaled Al <kalalwayly@gmail.com>
2026-07-02 00:31:36 -0500

    Add openTool function to navigate to tools

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -1,1 +1,15 @@
+function openTool(toolName) {
 
+    if (toolName === "budget") {
+        window.location.href = "budget.html";
+    }
+
+    if (toolName === "rent") {
+        window.location.href = "rent.html";
+    }
+
+    if (toolName === "checklist") {
+        window.location.href = "checklist.html";
+    }
+
+}
