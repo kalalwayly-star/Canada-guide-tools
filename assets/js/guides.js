9f8b66feed2edc417bfbe101562e551fc9cbf8ac
Kaled Al <kalalwayly@gmail.com>
2026-07-02 22:49:45 -0500

    Update fetch path for categories.json file

--- assets/js/guides.js
+++ assets/js/guides.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     // Fetch categories database file entries dynamically from server paths
-    fetch("./assets/data/categories.json")
+    fetch("../assets/data/categories.json")
         .then(res => {
             if (!res.ok) {
                 throw new Error(`HTTP error! status code profile verified: ${res.status}`);
